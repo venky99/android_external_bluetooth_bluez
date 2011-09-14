52f72222bf26a733db838aad96ed23fdb362dc88
Srinivas Krovvidi <skrovvid@codeaurora.org>
2011-09-14 12:36:31 +0530

    Bluetooth: Avoiding NULL dereference in API audio_device_cancel_authorization
    Change-Id: I321e635d26f51045c3dd05d52a7f8f46d4776ba4
    CRs-Fixed: 306333

--- audio/device.c
+++ audio/device.c
@@ -828,8 +828,15 @@ int audio_device_request_authorization(struct audio_device *dev,
 int audio_device_cancel_authorization(struct audio_device *dev,
 					authorization_cb cb, void *user_data)
 {
-	struct dev_priv *priv = dev->priv;
+	struct dev_priv *priv;
 	GSList *l, *next;
+
+	if (dev)
+		priv = dev->priv;
+	else {
+		DBG("NULL argument for device!!!");
+		return 0;
+	}
 
 	for (l = priv->auths; l != NULL; l = next) {
 		struct service_auth *auth = l->data;
